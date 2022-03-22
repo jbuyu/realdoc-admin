5d633e31666197c76a641cdfafbd1364720da617
jbuyu <jbuyu@ymail.com>
2022-03-22 15:04:54 +0300

    adds routes

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,24 +1,48 @@
-import React from "react";
-import "./topbar.css";
-import SettingsIcon from "@mui/icons-material/Settings";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
-export default function TopBar() {
+function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
-    <div className="topbar">
-      <div className="topbarWrapper">
-        <div className="topLeft">
-          <img className="logo" src="./logo.png" alt="app_logo" />
-        </div>
-        <div className="topRight">
-          <div className="topbarIconContainer">
-            <AccountCircleIcon className="material-icon" />
-          </div>
-          <div className="topbarIconContainer">
-            <SettingsIcon className="material-icon" />
-          </div>
-        </div>
+    <header className="header">
+      <div className="topLeft">
+        <img className="logo" src="./logo.png" alt="app_logo" />
       </div>
-    </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
   );
 }
+
+export default Header;
