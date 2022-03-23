b434535687cfa45a0c65a69cded1a5f91bacec2a
jbuyu <jbuyu@ymail.com>
2022-03-23 18:50:11 +0300

    setting up diagnosis page

--- src/pages/consultationList/Consultation.js
+++ src/pages/consultationList/Consultation.js
@@ -41,83 +41,95 @@ export default function Consultation({ match }) {
   }, [dispatch, history, message, isError]);
   return (
     <div className="consultation">
-      <Card
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          paddingRight: "4rem",
-        }}
-      >
-        <Box
+      <div className="consultation-container">
+        <Card
           sx={{
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-around",
-            width: "50%",
+            justifyContent: "space-between",
+            paddingRight: "4rem",
+            width:"80%"
           }}
         >
-          <CardContent
+          <Box
             sx={{
-              flex: "1 0 auto",
-              justifyContent: "flex-start",
-              // width: "80%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+              width: "50%",
             }}
           >
-            <Typography
-              variant="subtitle1"
-              color="text.secondary"
-              component="div"
+            <CardContent
+              sx={{
+                flex: "1 0 auto",
+                justifyContent: "flex-start",
+                // width: "80%",
+              }}
             >
-              <div className="details-row">
-                <div className="details-label">Name:</div>
-                <div>
-                  {consultation &&
-                    consultation.firstName + "   " + consultation.lastName}
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                component="div"
+              >
+                <div className="details-row">
+                  <div className="details-label">Name:</div>
+                  <div>
+                    {consultation &&
+                      consultation.firstName + "   " + consultation.lastName}
+                  </div>
                 </div>
+                <div className="details-row">
+                  <div className="details-label">Phone:</div>
+                  <div>{consultation && consultation.phoneNo}</div>
+                </div>
+                <div className="details-row">
+                  <div className="details-label">Email:</div>
+                  <div>{consultation && consultation.email}</div>
+                </div>
+                <div className="details-row">
+                  <div className="details-label">DOB:</div>
+                  <div>{consultation && consultation.dateOfBirth}</div>
+                </div>
+                <div className="details-row">
+                  <div className="details-label">Type:</div>
+                  <div>{consultation && consultation.consultationType}</div>
+                </div>
+                <div className="details-row">
+                  <div className="details-label">Symptoms:</div>
+                  <div>{consultation && consultation.symptoms}</div>
+                </div>
+                <div className="details-row">
+                  <div className="details-label">Created on:</div>
+                  <div>{consultation && consultation.createdAt}</div>
+                </div>
+              </Typography>
+            </CardContent>
+          </Box>
+          {consultation && consultation.gender === "Male" ? (
+            <CardMedia
+              component="img"
+              sx={{ width: 150 }}
+              image="/male.png"
+              alt="male"
+            />
+          ) : (
+            <CardMedia
+              component="img"
+              sx={{ width: 150 }}
+              image="/female.png"
+              alt="female"
+            />
+          )}
+        </Card>
+        <h4 className="dignosis-header">Diagnosis</h4>
+          <form className="diagnosis-form">
+            <textarea>Diagnosis...</textarea>
+            <div className="diagnosis-btn-container">
+            <button className="btn" >
+                Update
+              </button>
               </div>
-              <div className="details-row">
-                <div className="details-label">Phone:</div>
-                <div>{consultation && consultation.phoneNo}</div>
-              </div>
-              <div className="details-row">
-                <div className="details-label">Email:</div>
-                <div>{consultation && consultation.email}</div>
-              </div>
-              <div className="details-row">
-                <div className="details-label">DOB:</div>
-                <div>{consultation && consultation.dateOfBirth}</div>
-              </div>
-              <div className="details-row">
-                <div className="details-label">Type:</div>
-                <div>{consultation && consultation.consultationType}</div>
-              </div>
-              <div className="details-row">
-                <div className="details-label">Symptoms:</div>
-                <div>{consultation && consultation.symptoms}</div>
-              </div>
-              <div className="details-row">
-                <div className="details-label">Created on:</div>
-                <div>{consultation && consultation.createdAt}</div>
-              </div>
-            </Typography>
-          </CardContent>
-        </Box>
-        {consultation && consultation.gender === "Male" ? (
-          <CardMedia
-            component="img"
-            sx={{ width: 150 }}
-            image="/male.png"
-            alt="male"
-          />
-        ) : (
-          <CardMedia
-            component="img"
-            sx={{ width: 150 }}
-            image="/female.png"
-            alt="female"
-          />
-        )}
-      </Card>
+          </form>
+      </div>
     </div>
   );
 }
