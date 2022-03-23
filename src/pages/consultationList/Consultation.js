301035ec90f794d9cbe354460bf3fc11c49a0443
jbuyu <jbuyu@ymail.com>
2022-03-23 16:17:07 +0300

    adds tabs

--- src/pages/consultationList/Consultation.js
+++ src/pages/consultationList/Consultation.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
-export default function Consultation({match}) {
-    let consultationId = match.params.id;
+export default function Consultation({ match }) {
+  let consultationId = match.params.id;
   return (
-    <div className='consultation'>{consultationId}</div>
-  )
+    <div className="consultation">
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 }
