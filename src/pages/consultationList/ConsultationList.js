9eab116aaec03ae6cc26ce4619f7ce28fa4a3a92
jbuyu <jbuyu@ymail.com>
2022-03-24 10:41:32 +0300

    shifts cells

--- src/pages/consultationList/ConsultationList.js
+++ src/pages/consultationList/ConsultationList.js
@@ -3,15 +3,14 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import Spinner from "../../components/Spinner";
-// import { format, compareAsc } from 'date-fns';
+import { format, compareAsc } from "date-fns";
 
-import {Link} from "react-router-dom"
-import "./consultation.css"
-
+import { Link } from "react-router-dom";
+import "./consultation.css";
 
 import {
   getConsultations,
-  reset
+  reset,
 } from "../../features/consultations/consultationSlice";
 import "./consultation.css";
 
@@ -30,23 +29,80 @@ export default function ConsultationList() {
 
   const columns = [
     // { field: "_id", headerName: "ID", width: 130 },
-    { field: "createdAt", headerName: "Date Created", width: 130 },
-    { field: "gender", headerName: "Gender", width: 130 },
-    { field: "firstName", headerName: "First name", width: 100 },
-    { field: "lastName", headerName: "Last name", width: 100 },
-    { field: "dateOfBirth", headerName: "DOB", width: 130 },
-    { field: "consultationType", headerName: "Type", width: 70 },
-    { field: "phoneNo", headerName: "Phone Number", width: 130 },
-    { field: "email", headerName: "Email", width: 130 },
-    { field: "symptoms", headerName: "Symptoms", width: 200 },
     {
-      field: "action",
-      headerName: "Action",
-      width: 200,
+      field: "createdAt",
+      headerName: "Date Created",
+      width: 130,
       renderCell: (params) => {
         return (
           <div className="cellAction">
-            <Link to={`/consultation/${params.row._id}`} style={{ textDecoration: "none" }}>
+            <div className="">
+              {format(new Date(params.row.createdAt), "MM/dd/yyyy")}
+            </div>
+          </div>
+        );
+      },
+    },
+    { field: "consultationType", headerName: "Type", width: 120 },
+    { field: "gender", headerName: "Gender", width: 70 },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 140,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <div className="">
+              {params.row.firstName + " " + params.row.lastName}
+            </div>
+          </div>
+        );
+      },
+    },
+    { field: "phoneNo", headerName: "Phone Number", width: 120 },
+    {
+      field: "dateOfBirth",
+      headerName: "DOB",
+      width: 130,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <div className="">
+              {format(new Date(params.row.dateOfBirth), "MM/dd/yyyy")}
+            </div>
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 130 },
+    { field: "symptoms", headerName: "Symptoms", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 140,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            {params.row.status === "pending" ? (
+              <div className="viewButton">PENDING</div>
+            ) : (
+              <div className="viewButton">COMPLETED</div>
+            )}
+          </div>
+        );
+      },
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 140,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link
+              to={`/consultation/${params.row._id}`}
+              style={{ textDecoration: "none" }}
+            >
               <div className="viewButton">view</div>
             </Link>
           </div>
